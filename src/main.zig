7901241e6da5dad1e8f03bd6dc48244cfea16e5a
Piotr Mikolajczyk <piotrmikola@pm.me>
2024-05-01 18:59:47 +0200

    Change result to be saved into a file

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 
-pub fn main() !void {
+fn generate_image(writer: std.fs.File.Writer) !void {
     const image_width: u16 = 256;
     const image_height: u16 = 256;
 
-    var stdout = std.io.getStdOut().writer();
-    try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
+    try writer.print("P3\n{} {}\n255\n", .{ image_width, image_height });
 
     for (0..image_height) |j| {
         for (0..image_width) |i| {
@@ -17,7 +16,14 @@ pub fn main() !void {
             const ig = @as(u16, @intFromFloat(255.999 * g));
             const ib = @as(u16, @intFromFloat(255.999 * b));
 
-            try stdout.print("{} {} {}\n", .{ ir, ig, ib });
+            try writer.print("{} {} {}\n", .{ ir, ig, ib });
         }
     }
+}
+pub fn main() !void {
+    const image_file = try std.fs.cwd().createFile("image.ppm", .{});
+    defer image_file.close();
+
+    const file_writer = image_file.writer();
+    try generate_image(file_writer);
 }
