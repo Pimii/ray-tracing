28b542f3123dc39ff8f67ec1c34712977f08896b
Piotr Mikolajczyk <piotrmikola@pm.me>
2024-05-02 20:32:04 +0200

    Add utility functions

--- src/vec3.zig
+++ src/vec3.zig
@@ -43,3 +43,44 @@ pub const Vec3 = struct {
 };
 
 pub const Point3 = Vec3;
+
+pub fn add_vectors(vec1: Vec3, vec2: Vec3) Vec3 {
+    return Vec3{
+        .x = vec1.x + vec2.x,
+        .y = vec1.y + vec2.y,
+        .z = vec1.z + vec2.z,
+    };
+}
+
+pub fn subtract_vectors(vec1: Vec3, vec2: Vec3) Vec3 {
+    return Vec3{
+        .x = vec1.x - vec2.x,
+        .y = vec1.y - vec2.y,
+        .z = vec1.z - vec2.z,
+    };
+}
+
+pub fn multiply_vectors(vec1: Vec3, vec2: Vec3) Vec3 {
+    return Vec3{
+        .x = vec1.x * vec2.x,
+        .y = vec1.y * vec2.y,
+        .z = vec1.z * vec2.z,
+    };
+}
+
+pub fn dot(vec1: Vec3, vec2: Vec3) f64 {
+    return (vec1.x * vec2.x) + (vec1.y * vec2.y) + (vec1.z * vec2.z);
+}
+
+pub fn cross(vec1: Vec3, vec2: Vec3) Vec3 {
+    return Vec3{
+        .x = (vec1.y * vec2.z) - (vec1.z * vec2.y),
+        .y = (vec1.z * vec2.x) - (vec1.x * vec2.z),
+        .z = (vec1.x * vec2.y) - (vec1.y * vec2.x),
+    };
+}
+
+pub fn unit_vector(vec: Vec3) Vec3 {
+    vec.divide(vec.len());
+    return vec;
+}
