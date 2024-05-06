721e6e06de3b640d6700a7e5441982ddecb3f123
Piotr Mikolajczyk <piotrmikola@pm.me>
2024-05-06 22:18:30 +0200

    Render surface normals

--- src/render.zig
+++ src/render.zig
@@ -1,27 +1,39 @@
-const Vec = @import("vec3.zig");
-const Point3 = Vec.Point3;
+const vec = @import("vec3.zig");
+const Point3 = vec.Point3;
 const Color = @import("color.zig").Color;
 const Ray = @import("ray.zig").Ray;
+const math = @import("std").math;
 
-pub fn hit_sphere(center: Point3, radius: f64, ray: Ray) bool {
-    const oc = Vec.subtract_vectors(center, ray.origin);
-    const a = Vec.dot(ray.direction, ray.direction);
-    const b = -2.0 * Vec.dot(ray.direction, oc);
-    const c = Vec.dot(oc, oc) - radius * radius;
+fn hit_sphere(center: Point3, radius: f64, ray: Ray) f64 {
+    const oc = vec.subtract_vectors(center, ray.origin);
+    const a = vec.dot(ray.direction, ray.direction);
+    const b = -2.0 * vec.dot(ray.direction, oc);
+    const c = vec.dot(oc, oc) - radius * radius;
     const discriminant = b * b - 4 * a * c;
-    return discriminant >= 0;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - math.sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
-pub fn ray_color(ray: Ray) Color.Color {
-    if (hit_sphere(Point3.init(0, 0, -1), 0.5, ray)) {
-        return Color.Color.init(1, 0, 0);
-    }
-    const unit_direction = Vec.unit_vector(ray.direction);
+fn background_color(ray: Ray) Color {
+    const unit_direction = vec.unit_vector(ray.direction);
     const alpha = 0.5 * (unit_direction.y + 1.0);
     const lerp = blk: {
-        var lerp = Vec.multiply_vector(1.0 - alpha, Color.Color.init(1.0, 1.0, 1.0));
-        lerp.add(Vec.multiply_vector(alpha, Color.Color.init(0.1, 0.8, 0.8)));
+        var lerp = vec.multiply_vector(1.0 - alpha, Color.init(1.0, 1.0, 1.0));
+        lerp.add(vec.multiply_vector(alpha, Color.init(0.3, 0.6, 0.9)));
         break :blk lerp;
     };
     return lerp;
+}
+
+pub fn ray_color(ray: Ray) Color {
+    const t = hit_sphere(Point3.init(0, 0, -1), 0.5, ray);
+    if (t > 0.0) {
+        const n = vec.unit_vector(vec.subtract_vectors(ray.at(t), vec.Vec3.init(0, 0, -1)));
+        return vec.multiply_vector(0.5, Color.init(n.x + 1, n.y + 1, n.z + 1));
+    }
+    return background_color(ray);
 }
